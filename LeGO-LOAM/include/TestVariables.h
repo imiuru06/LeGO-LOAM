8020241092f9648076f78f0352f81ea5299bc41c
imiuru06 <imiunu06@gmail.com>
2020-04-16 14:58:42 +0900

    Declare Variables for test

---
+++ LeGO-LOAM/include/TestVariables.h
@@ -0,0 +1,17 @@
+// this headfile exist for the only testing.
+//
+
+#ifdef TESTVARIABLES_H_
+#define TESTVARIABLES_H_
+
+
+float maxAngRad = 0;
+float minAngRad = 0;
+float maxAngDeg = 0;
+float minAngDeg = 0;
+
+
+
+int cntTest = 0;
+
+#endif
